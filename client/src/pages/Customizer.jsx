c51b78c5005d9170a3cf38cea84f6043bced4c8c
Mirza Sahin <salihmirza123@gmail.com>
2024-02-21 16:51:18 +0300

    Add customizable color picker functionality

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,17 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
-import { useSnapshot } from 'valtio'
+import React, { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
 
-import config from '../config/config'
-import state from '../store'
-import { download } from '../assets'
-import { downloadCanvasToImage, reader } from '../config/helpers'
-import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants'
-import { fadeAnimation, slideAnimation } from '../config/motion'
-import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components'
+import config from "../config/config";
+import state from "../store";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+import {
+  AIPicker,
+  ColorPicker,
+  CustomButton,
+  FilePicker,
+  Tab,
+} from "../components";
 
 const Customizer = () => {
-  const snap = useSnapshot(state)
+  const snap = useSnapshot(state);
+
+  const [file, setfile] = useState("");
+
+  const [prompt, setprompt] = useState("");
+  const [generatingImg, setgeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setactiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  // show tab content depending on the activeTab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+
+      case "filepicker":
+        return <FilePicker />;
+
+      case "aipicker":
+        return <AIPicker />;
+
+      default:
+        return null;
+    }
+  };
 
   return (
     <AnimatePresence>
@@ -19,38 +53,37 @@ const Customizer = () => {
         <>
           <motion.div
             key="custom"
-            className='absolute top-0 left-0 z-10'
-            {...slideAnimation('left')}
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
           >
-            <div className='flex items-center min-h-screen'>
-              <div className='editortabs-container tabs'>
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                  <Tab
-                    key={tab.name}
-                    tab={tab}
-                    handleClick={() => { }}
-                  />
+                  <Tab key={tab.name} tab={tab} handleClick={() => {setActiveEditorTab(tab.name)}} />
                 ))}
 
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
 
           <motion.div
-            className='absolute z-10 top-5 right-5'
+            className="absolute z-10 top-5 right-5"
             {...fadeAnimation}
           >
             <CustomButton
               type="filled"
               title="Go Back"
-              handleClick={() => { state.intro = true }}
+              handleClick={() => {
+                state.intro = true;
+              }}
               customStyles="w-fit px-4 py-2.5 font-bold text-sm"
             />
           </motion.div>
 
           <motion.div
-            className='filtertabs-container'
-            {...slideAnimation('up')}
+            className="filtertabs-container"
+            {...slideAnimation("up")}
           >
             {FilterTabs.map((tab) => (
               <Tab
@@ -58,15 +91,14 @@ const Customizer = () => {
                 tab={tab}
                 isFilterTab
                 isActiveTab=""
-                handleClick={() => { }}
+                handleClick={() => {}}
               />
             ))}
-
           </motion.div>
         </>
       )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default Customizer
+export default Customizer;
